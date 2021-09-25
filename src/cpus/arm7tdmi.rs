b74dfcd1ee9074042cf0383921d4c213eab12609
TornaxO7 <tornax07@gmail.com>
2021-09-25 16:54:20 +0200

    Adding operating states for the ARM7TDMi

--- src/cpus/arm7tdmi.rs
+++ src/cpus/arm7tdmi.rs
@@ -1,11 +1,21 @@
 use crate::ram::Ram;
 
+use crate::ram::memory_size::{Halfword, Word};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum OperatingState {
+    Arm,
+    Thumb,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Arm7TDMI {
+
 }
 
 impl Arm7TDMI {
     pub fn fetch(&mut self, ram: &Ram) {
+
     }
 
     pub fn decode(&self) {
