3f064811f82c6463794438295b991b549c33a4d6
TornaxO7 <tornax07@gmail.com>
2021-10-03 23:00:12 +0200

    InstructionMap => BranchOperand
    Added tests

--- src/cpus/general/instruction_map/branch/operand.rs
+++ src/cpus/general/instruction_map/branch/operand.rs
@@ -29,3 +29,28 @@ impl BranchOperand {
             && (instruction_val >> 4) & 0b1111 == 0b0001
     }
 }
+
+#[cfg(test)]
+mod tests {
+    
+    use super::BranchOperand;
+    use crate::cpus::general::instruction::Instruction;
+
+    #[test]
+    fn test_is_b_or_bl_instruction() {
+        let b_instruction = Instruction::from(0b0000_1010_0000_0000_0000_0000_0000_0000);
+        let bl_instruction = Instruction::from(0b0000_1011_0000_0000_0000_0000_0000_0000);
+
+        let invalid_instruction = Instruction::from(0b101010);
+
+        assert!(BranchOperand::is_b_or_bl_instruction(&b_instruction));
+        assert!(BranchOperand::is_b_or_bl_instruction(&bl_instruction));
+        assert!(!BranchOperand::is_bx_instruction(&invalid_instruction));
+    }
+
+    #[test]
+    fn test_is_bx_instruction() {
+        let bx_instruction = Instruction::from(0b0000_0001_0010_1111_1111_1111_0001_0000);
+        assert!(BranchOperand::is_bx_instruction(&bx_instruction));
+    }
+}
