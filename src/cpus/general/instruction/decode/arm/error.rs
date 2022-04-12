ccfd5c0194e0bfbe7f8d7885b1e70bd6a1d03a0f
TornaxO7 <tornax07@gmail.com>
2022-04-12 02:27:19 +0200

    removing the decode directory

--- src/cpus/general/instruction/decode/arm/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(thiserror::Error, Debug, Clone, PartialEq, Eq)]
-pub enum ArmDecodeError {
-    #[error("[Arm decode]: Undefined instruction: {0:b}")]
-    UndefinedInstruction(u32),
-
-    #[error("[Arm decode]: Reached unpredictable instruction: {0:b}")]
-    UnpredictableInstruction(u32),
-}
--- src/cpus/general/instruction/decode/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod arm;
-mod decode_data;
-mod thumb;
-
-pub use decode_data::DecodeData;
-pub use thumb::ThumbDecode;
