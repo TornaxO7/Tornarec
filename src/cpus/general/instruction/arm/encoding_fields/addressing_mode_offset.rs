90726e062ccf656952351fd619a8d57c06edeee1
TornaxO7 <tornax07@gmail.com>
2022-04-12 19:44:08 +0200

    adding functions for addressing mode offset 2 with test

--- src/cpus/general/instruction/arm/encoding_fields/addressing_mode_offset.rs
+++ src/cpus/general/instruction/arm/encoding_fields/addressing_mode_offset.rs
@@ -31,10 +31,7 @@ impl AddressingMode1Offset {
         let rotate_imm = u8::try_from((word >> 8) & 0b1111).unwrap();
         let immed8 = u8::try_from(word & 0b1111_1111).unwrap();
 
-        Self::Immediate {
-            rotate_imm,
-            immed8,
-        }
+        Self::Immediate { rotate_imm, immed8 }
     }
 
     pub fn get_immediate_shift(word: Word) -> Self {
@@ -54,11 +51,7 @@ impl AddressingMode1Offset {
         let shift = u8::try_from((word >> 5) & 0b11).unwrap();
         let rm = u8::try_from(word & 0b1111).unwrap();
 
-        Self::RegisterShift {
-            rs,
-            shift,
-            rm,
-        }
+        Self::RegisterShift { rs, shift, rm }
     }
 }
 
@@ -71,6 +64,31 @@ pub enum AddressingMode2Offset {
         shift: u8,
         rm: Register,
     },
+}
+
+impl AddressingMode2Offset {
+    pub fn get_immediate_offset(word: Word) -> Self {
+        let immed_offset = u16::try_from(word & 0b1111_1111_1111).unwrap();
+        Self::ImmediateOffset(immed_offset)
+    }
+
+    pub fn get_register_offset(word: Word) -> Self {
+        let rm = Register::try_from(word & 0b1111).unwrap();
+
+        Self::RegisterOffset(rm)
+    }
+
+    pub fn get_scaled_register_offset(word: Word) -> Self {
+        let shift_imm = u8::try_from((word >> 7) & 0b1111).unwrap();
+        let shift = u8::try_from((word >> 5) & 0b11).unwrap();
+        let rm = Register::try_from(word & 0b1111).unwrap();
+
+        Self::ScaledRegisterOffset {
+            shift_imm,
+            shift,
+            rm,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -107,37 +125,85 @@ pub enum AddressingMode5Offset {
 
 #[cfg(test)]
 mod tests {
+    use crate::cpus::general::instruction::arm::{
+        encoding_fields::AddressingMode2Offset,
+        Register,
+    };
+
     use super::AddressingMode1Offset;
 
     #[test]
     fn addressing_mode_1_offset_immediate() {
         let word = 0b0000_0000_0000_0000_0000_1111_1111_1111;
 
-        assert_eq!(AddressingMode1Offset::Immediate {
-            rotate_imm: 0b1111_1111,
-            immed8: 0b1111
-        }, AddressingMode1Offset::get_immediate(word));
+        assert_eq!(
+            AddressingMode1Offset::Immediate {
+                rotate_imm: 0b1111_1111,
+                immed8: 0b1111
+            },
+            AddressingMode1Offset::get_immediate(word)
+        );
     }
 
     #[test]
     fn addressing_mode_1_offset_immediate_shift() {
         let word = 0b0000_0000_0000_0000_0000_1111_110_1111;
 
-        assert_eq!(AddressingMode1Offset::ImmediateShift {
-            shift_imm: 0b1111,
-            shift: 0b11,
-            rm: 0b1111
-        }, AddressingMode1Offset::get_immediate_shift(word));
+        assert_eq!(
+            AddressingMode1Offset::ImmediateShift {
+                shift_imm: 0b1111,
+                shift: 0b11,
+                rm: 0b1111
+            },
+            AddressingMode1Offset::get_immediate_shift(word)
+        );
     }
 
     #[test]
     fn addressing_mode_1_offset_register_shift() {
         let word = 0b0000_0000_0000_0000_0000_1111_0_11_1_1111;
 
-        assert_eq!(AddressingMode1Offset::RegisterShift {
-            rs: 0b1111,
-            shift: 0b11,
-            rm: 0b1111,
-        }, AddressingMode1Offset::get_register_shift(word));
+        assert_eq!(
+            AddressingMode1Offset::RegisterShift {
+                rs: 0b1111,
+                shift: 0b11,
+                rm: 0b1111,
+            },
+            AddressingMode1Offset::get_register_shift(word)
+        );
+    }
+
+    #[test]
+    fn addresing_mode_2_immediate_offset() {
+        let word = 0b0000_0000_0000_0000_0000_1111_1111_1111;
+
+        assert_eq!(
+            AddressingMode2Offset::get_immediate_offset(0b1111_1111_1111),
+            AddressingMode2Offset::ImmediateOffset(0b1111_1111_1111)
+        );
+    }
+
+    #[test]
+    fn addressing_mode_2_register_offset() {
+        let word = 0b0000_0000_0000_0000_0000_0000_0000_1111;
+
+        assert_eq!(
+            AddressingMode2Offset::get_register_offset(word),
+            AddressingMode2Offset::RegisterOffset(Register::from(0b1111))
+        );
+    }
+
+    #[test]
+    fn addressing_mode_2_scaled_register_offset() {
+        let word = 0b0000_0000_0000_0000_0000_1111_1110_1111;
+
+        assert_eq!(
+            AddressingMode2Offset::get_scaled_register_offset(word),
+            AddressingMode2Offset::ScaledRegisterOffset {
+                shift_imm: 0b1111,
+                shift: 0b11,
+                rm: Register::from(0b1111)
+            }
+        );
     }
 }
