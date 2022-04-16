832945f0f28530c13d630a009b132dd1f9e7ac26
TornaxO7 <tornax07@gmail.com>
2022-04-16 18:16:39 +0200

    arm-operand: adding tests for load_store_coprocessor.rs

--- src/cpus/general/instruction/arm/operand/load_store_coprocessor.rs
+++ src/cpus/general/instruction/arm/operand/load_store_coprocessor.rs
@@ -29,7 +29,7 @@ pub fn get_ldc_stc_operand(value: Word) -> ArmOperand {
     }
 }
 
-pub fn getd_mcr_mrc_operand(value: Word) -> ArmOperand {
+pub fn get_mcr_mrc_operand(value: Word) -> ArmOperand {
     ArmOperand::MCRandMRC {
         opcode1: u8::try_from((value >> 21) & 0b111).unwrap(),
         crn: Register::new(value, 16, 0b1111),
@@ -75,7 +75,18 @@ impl From<Word> for LoadStoreCoprocessorMode {
 
 #[cfg(test)]
 mod test {
-    use crate::cpus::general::instruction::arm::operand::load_store_coprocessor::LoadStoreCoprocessorMode;
+    use crate::cpus::general::instruction::arm::{
+        operand::{
+            load_store_coprocessor::{
+                get_ldc_stc_operand,
+                get_mcr_mrc_operand,
+                LoadStoreCoprocessorMode, get_mcrr_mrrc_operand,
+            },
+            ArmOperand,
+        },
+        types::Register,
+        BitState,
+    };
 
     #[test]
     fn test_load_store_coprocessor_mode() {
@@ -102,6 +113,58 @@ mod test {
         assert_eq!(
             LoadStoreCoprocessorMode::ImmediateOffset,
             LoadStoreCoprocessorMode::from(immediate_offset)
+        );
+    }
+
+    #[test]
+    fn test_get_ldc_stc_operand() {
+        let value = 0b0000_1101_1110_1111_1111_1111_1111_1111;
+
+        assert_eq!(
+            ArmOperand::LDCandSTC {
+                u: BitState::SET,
+                n: BitState::SET,
+                rn: Register::from(0b1111),
+                crd: Register::from(0b1111),
+                cp_num: 0b1111,
+                immed8: 0b1111_1111,
+                // it has its own tests
+                mode: LoadStoreCoprocessorMode::from(value),
+            },
+            get_ldc_stc_operand(value)
+        );
+    }
+
+    #[test]
+    fn test_get_mcr_mrc_operand() {
+        let value = 0b0000_1110_1110_1111_1111_1111_1111_1111;
+
+        assert_eq!(
+            ArmOperand::MCRandMRC {
+                opcode1: 0b111,
+                crn: Register::from(0b1111),
+                rd: Register::from(0b1111),
+                cp_num: 0b1111,
+                opcode2: 0b111,
+                crm: Register::from(0b1111)
+            },
+            get_mcr_mrc_operand(value)
+        );
+    }
+
+    #[test]
+    fn test_get_mcrr_mrrc_operand() {
+        let value = 0b0000_1100_0100_1111_1111_1111_1111_1111;
+
+        assert_eq!(
+            ArmOperand::MCRRandMRRC {
+                rn: Register::from(0b1111),
+                rd: Register::from(0b111),
+                cp_num: 0b1111,
+                opcode: 0b1111,
+                crm: Register::from(0b111),
+            },
+            get_mcrr_mrrc_operand(value)
         );
     }
 }
