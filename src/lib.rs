fa8fdd74a9c55b063d5610207893c1be7fe4c573
TornaxO7 <tornax07@gmail.com>
2022-09-30 04:07:40 +0200

    adding screens to nds

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+use nds_components::screen::NDSScreens;
+
 pub mod cpus;
 pub mod jit;
 pub mod nds_components;
 
 pub type Addr = usize;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug)]
 pub struct NDS {
     mem: Vec<u8>,
+    screens: NDSScreens,
 }
--- src/nds_components/screen/mod.rs
+++ src/nds_components/screen/mod.rs
@@ -1,6 +1,14 @@
 pub mod base;
 pub mod touchscreen;
 
+use self::{base::BaseScreen, touchscreen::Touchscreen};
+
+#[derive(Debug)]
+pub struct NDSScreens {
+    pub top: BaseScreen,
+    pub bottom: Touchscreen,
+}
+
 pub trait Screen {
     fn new<S: AsRef<str>>(name: S) -> Self;
 
